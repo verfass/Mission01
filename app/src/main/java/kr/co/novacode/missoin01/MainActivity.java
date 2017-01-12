c159a875b133a57431adc908898620f8c3c59168
verfass <verfass@gmail.com>
2017-01-12 19:18:05 +0900

    두번째 커밋

--- app/src/main/java/kr/co/novacode/missoin01/MainActivity.java
+++ app/src/main/java/kr/co/novacode/missoin01/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         changeImage(1);
 
+
         button01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
